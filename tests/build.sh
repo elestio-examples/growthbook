b24ecfbfdeafca02e9206c06f483685193825568
Prem chavhan <pchavhan@logiquad.com>
2023-09-13 19:12:29 +0530

    fixes

--- tests/build.sh
+++ tests/build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 rm -f docker-compose.yml
+rm -f Dockerfile
 mv docker-compose-new.yml docker-compose.yml
+mv Dockerfile-new Dockerfile
 docker buildx build . --output type=docker,name=elestio4test/growthbook:latest | docker load
